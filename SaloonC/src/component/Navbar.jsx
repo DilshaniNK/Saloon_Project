e16972ee62d47ee45bb9ee873db3628aeba9bb31
D.N.Kodithuwakku <140165465+DilshaniNK@users.noreply.github.com>
2025-02-22 08:54:13 +0530

    start to create navbar and add react icons

--- SaloonC/src/component/Navbar.jsx
+++ SaloonC/src/component/Navbar.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { MdClose } from "react-icons/md";
+import { HiMenuAlt3, HiSearch } from "react-icons/hi";
+import { PiShoppingCartLight } from "react-icons/pi";
+
 
 export default function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+  const [dropDown, setDropDown] = useState(false);
+
+  const showDropDown = () =>{
+    setDropDown(!dropDown);
+
+  };
+
+  return <nav>
+    <div>
+        <div>
+            <img src='/logo.png' alt='' width={156}/>
+            <span>
+                {dropDown ? (
+                    <div onClick={showDropDown}>
+                        <MdClose />
+                    </div>
+                ):(
+                    <div onClick={showDropDown}>
+                        <HiMenuAlt3/>
+                    </div>
+                )}
+                <HiSearch size={24}/>
+                <PiShoppingCartLight size={24} />
+            </span>
+        </div>  
+        
+    </div>
+  </nav>
 }
